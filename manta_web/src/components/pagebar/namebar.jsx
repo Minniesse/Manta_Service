50959e4d8ee9aa08a642a2f376bce0b7e0b081a0
Minniesse <pkongkae@cmkl.ac.th>
2024-04-06 01:56:21 +0700

    Add appearance icon (temp) + framer motion

--- manta_web/src/components/pagebar/namebar.jsx
+++ manta_web/src/components/pagebar/namebar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import style from './namebar.module.css';
 import { useLocation } from 'react-router-dom';
+import { MotionValue, motion, useMotionValue, useSpring, useTransform, useAnimation } from "framer-motion";
 
 function PageBox () {
     const location = useLocation();
@@ -14,11 +15,19 @@ function PageBox () {
         content = 'Page';
       }
   return (
-    <div
-      className={style.pageBoxStyle}
-    >
-      {content}
+    <>
+    <div className={style.LeftBar}>
+      <div className={style.pageBoxStyle}>
+        {content}
+      </div>
+      <motion.div
+        whileHover={{ scale: 0.9 }}
+        whileTap={{ scale: 1.3, borderRadius: "100%"}}
+      >
+      <img src="/src/assets/appearance.svg" alt="Icon" />
+      </motion.div>
     </div>
+    </>
   );
 };
 
