3825854f84cadfdb2599d7ab4db38c575c58122f
PeachIsPeachy <one12jess@gmail.com>
2024-04-12 02:49:32 +0700

    completed landing page

--- manta_web/src/pages/LandingPage/Landing.jsx
+++ manta_web/src/pages/LandingPage/Landing.jsx
@@ -1,8 +1,45 @@
-import style from './Landing.module.css';
-import React from 'react'
+import style from "./Landing.module.css";
+import React from "react";
+import Map from "../../asset/Map.svg";
+import logoSquare from "../../asset/logoSquare.svg";
+import clickButton from "../../asset/clickButton.svg";
 
 export default function Landing() {
   return (
-    <h1>Landing</h1>
-  )
+    <div className="container">
+      <div className="content">
+        <ul>
+          <li>
+            <h1>Manta</h1>
+          </li>
+          <li>
+            <p>The highest level of care for Thailand's roads</p>
+          </li>
+          <li>
+            <img
+              src={logoSquare}
+              alt="logo"
+              style={{ width: "150px", height: "150px" }}
+            />
+          </li>
+          <li>
+            <img
+              src={clickButton}
+              alt="click button"
+              style={{ width: "200px", height: "50", cursor: "pointer" }}
+            />
+          </li>
+
+          <ul>
+            <li>Privacy</li>
+            <li>Terms of Use</li>
+            <li>About Us</li>
+          </ul>
+        </ul>
+      </div>
+      <div className="map-container ">
+        <img src={Map} alt="map" style={{ width: "520px", height: "520px" }} />
+      </div>
+    </div>
+  );
 }
