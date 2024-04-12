b5a67012414b07cc5c6c10574b0b9dfb554695a6
Minniesse <pkongkae@cmkl.ac.th>
2024-04-13 04:01:42 +0700

    Create component of the cctvdetails

--- manta_web/src/components/CCTVDetails/CameraDetails.jsx
+++ manta_web/src/components/CCTVDetails/CameraDetails.jsx
@@ -1,15 +1,38 @@
 import style from './Cameradetails.module.css';
 import React from 'react';
+import cctvstreamingdummy from '../../assets/cctvstreamingdummy.svg';
 
 const CameraDetails = ({ camera }) => {
     return (
         <div className={style.details}>
-            {/* <h2>{camera.model}</h2>
-            <p>Status: {camera.status}</p>
-            <p>IP: {camera.camera_he}</p> */}
             <div className={style.cctvname}>
-                <h2>Camera Info</h2>
-                <p>{camera.model}</p>
+                <div className={style.cctvicon}></div>
+                <div className={style.cctvnamebox}>
+                    <div className={style.blockElement}>
+                        {camera.model}
+                    </div>
+                    <div className={style.tags}>
+                        {camera.status}
+                    </div>
+                </div>
+            </div>
+            <div className={style.cctvinfo}>
+                <div className={style.left}>
+                    <div className={style.cctvstreaming}>
+                        <img src={cctvstreamingdummy} alt="cctvstreaming" className={style.cctvstreamingvideo} />
+                    </div>
+                    <div className={style.cctvinfocontentleft}>
+                            <div className={style.temp}>
+                                <h2>Camera Info</h2>
+                            </div>
+                            <div className={style.temp}>
+                                <p>Camera IP: {camera.ip}</p>
+                            </div>
+                            <div className={style.temp}>
+                                <p>Camera Location: {camera.location}</p>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
