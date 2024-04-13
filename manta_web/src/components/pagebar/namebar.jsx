d00d55e58c941624e701aeb0aaa4c155cec28027
Minniesse <pkongkae@cmkl.ac.th>
2024-04-13 07:18:57 +0700

    Set a condition that appearance icon will show on map page only

--- manta_web/src/components/pagebar/namebar.jsx
+++ manta_web/src/components/pagebar/namebar.jsx
@@ -6,8 +6,6 @@ import { motion } from "framer-motion";
 function PageBox ({ toggleMapType }) {
   const location = useLocation();
   const [isTabVisible, setIsTabVisible] = useState(false);
-  
-  
 
   let content;
   if (location.pathname === '/') {
@@ -18,7 +16,7 @@ function PageBox ({ toggleMapType }) {
   else if (location.pathname === '/CCTVConfig') {
     content = 'CCTV Config';
   } else {
-    content = 'Page';
+    content = 'CCTV Config';
   }
 
   const container = {
@@ -56,40 +54,44 @@ function PageBox ({ toggleMapType }) {
         <div className={style.pageBoxStyle}>
           {content}
         </div>
-        <motion.div
-          whileHover={{ scale: 0.9 }}
-          whileTap={{ scale: 1.3, rotate: 10, rotateX: -10 }}
-          onClick={() => setIsTabVisible(!isTabVisible)}
-        >
-          <img src="/src/assets/appearance.svg" alt="Icon" />
-        </motion.div>
+        {location.pathname === '/OverviewMaps' && (
+          <>
+            <motion.div
+              whileHover={{ scale: 0.9 }}
+              whileTap={{ scale: 1.3, rotate: 10, rotateX: -10 }}
+              onClick={() => setIsTabVisible(!isTabVisible)}
+            >
+              <img src="/src/assets/appearance.svg" alt="Icon" />
+            </motion.div>
+            <motion.div
+              className={style.notificationTab}
+              variants={container}
+              initial="hidden"
+              animate={isTabVisible ? "visible" : "hidden"}
+            >
+              <motion.div className={style.title} variants={item}>Choose the Appearance</motion.div>
+              <div className={style.itemsContainer}>
+                <motion.div className={style.item1} variants={item} onClick={() => toggleMapType('satellite')}>
+                  <img src="/src/assets/tempIcon.svg" alt="Icon" />
+                  <span>Satellite</span>
+                </motion.div>
+                <motion.div className={style.item2} variants={item}>
+                  <img src="/src/assets/tempIcon.svg" alt="Icon" />
+                  <span>Heatmap</span>
+                </motion.div>
+                <motion.div className={style.item3} variants={item} onClick={() => toggleMapType('Traffic')}>
+                  <img src="/src/assets/tempIcon.svg" alt="Icon" />
+                  <span>Traffic</span>
+                </motion.div>
+                <motion.div className={style.item4} variants={item}>
+                  <img src="/src/assets/tempIcon.svg" alt="Icon" />
+                  <span>Vehicles</span>
+                </motion.div>
+              </div>
+            </motion.div>
+          </>
+        )}
       </div>
-      <motion.div
-        className={style.notificationTab}
-        variants={container}
-        initial="hidden"
-        animate={isTabVisible ? "visible" : "hidden"}
-      >
-        <motion.div className={style.title} variants={item}>Choose the Appearance</motion.div>
-        <div className={style.itemsContainer}>
-          <motion.div className={style.item1} variants={item} onClick={() => toggleMapType('satellite')}>
-            <img src="/src/assets/tempIcon.svg" alt="Icon" />
-            <span>Satellite</span>
-          </motion.div>
-          <motion.div className={style.item2} variants={item}>
-            <img src="/src/assets/tempIcon.svg" alt="Icon" />
-            <span>Heatmap</span>
-          </motion.div>
-          <motion.div className={style.item3} variants={item} onClick={() => toggleMapType('Traffic')}>
-            <img src="/src/assets/tempIcon.svg" alt="Icon" />
-            <span>Traffic</span>
-          </motion.div>
-          <motion.div className={style.item4} variants={item}>
-            <img src="/src/assets/tempIcon.svg" alt="Icon" />
-            <span>Vehicles</span>
-          </motion.div>
-        </div>
-      </motion.div>
     </>
   );
 }
