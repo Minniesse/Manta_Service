c7d84990b079964871cff9436c45032e70793cf2
Minniesse <pkongkae@cmkl.ac.th>
2024-04-15 10:41:02 +0700

    Landing Page

--- manta_web/src/pages/LandingPage/Landing.jsx
+++ manta_web/src/pages/LandingPage/Landing.jsx
@@ -1,45 +1,48 @@
 import style from "./Landing.module.css";
 import React from "react";
-import Map from "../../asset/Map.svg";
-import logoSquare from "../../asset/logoSquare.svg";
-import clickButton from "../../asset/clickButton.svg";
+import Map from "../../assets/Map.svg";
+import logoSquare from "../../assets/logoSquare.svg";
+import clickButton from "../../assets/clickButton.svg";
+import Lottie from 'react-lottie';
+import animationData from '../..//assets/Maps_Lottie'
+import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate('/OverviewMaps');
+  }
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
   return (
-    <div className="container">
-      <div className="content">
-        <ul>
-          <li>
-            <h1>Manta</h1>
-          </li>
-          <li>
-            <p>The highest level of care for Thailand's roads</p>
-          </li>
-          <li>
-            <img
-              src={logoSquare}
-              alt="logo"
-              style={{ width: "150px", height: "150px" }}
+    <>
+      <div className={style.entire}>
+        <div className={style.Left}>
+          <h1 className={style.title}>Manta</h1>
+          <p className={style.subtitle}>The highest level <br /> of care for thailand’s roads</p>
+          <img src={logoSquare} alt="Map" className={style.map} />
+          <div className={style.button} onClick={handleButtonClick}>
+            <img src={clickButton} alt="Button" className={style.button} />
+          </div>
+          <div className={style.info}>
+            <p className={style.buttonText}>Privacy</p>
+            <p className={style.buttonText}>Terms of Use</p>
+            <p className={style.buttonText}>About Us</p>
+          </div>
+        </div>
+        <div className={style.Right}>
+            <Lottie options={defaultOptions}
+            height={550}
+            width={550}
             />
-          </li>
-          <li>
-            <img
-              src={clickButton}
-              alt="click button"
-              style={{ width: "200px", height: "50", cursor: "pointer" }}
-            />
-          </li>
-
-          <ul>
-            <li>Privacy</li>
-            <li>Terms of Use</li>
-            <li>About Us</li>
-          </ul>
-        </ul>
+        </div>
       </div>
-      <div className="map-container ">
-        <img src={Map} alt="map" style={{ width: "520px", height: "520px" }} />
-      </div>
-    </div>
+    </>
   );
 }
